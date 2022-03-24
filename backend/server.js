90aa2511fcbad7ff7633c666a47fcaafe6c09a14
Smrutiranjan Patra <76831153+Smrutiranjan-Patra@users.noreply.github.com>
2022-03-24 16:59:53 +0530

    update

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-const DBconnect = require("./config/db");
-const app = require("./app");
-const Port = process.env.PORT || 2022;
-
-app.listen(Port, async function () {
-    await DBconnect();
-    console.log(`Started at http://localhost:${Port}`);
-})
